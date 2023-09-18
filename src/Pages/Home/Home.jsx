a92ec30a3511618e969f4fe58afeebb05f41650a
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-09-18 10:04:27 +0600

    hero header section style with daisy ui

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            home
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero-container lg:flex-row-reverse">
+          <img src="https://i.ibb.co/KbKcy7z/Images.png" className="img" alt="" />
+          <div className="hero-info">
+            <h1 className="text-5xl font-bold">Learn with expert anytime anywhere</h1>
+            <p className="py-6">
+              Our mision is to help people to find the best course online and learn with expert anytime, anywhere.
+            </p>
+            <button className="btn btn-primary">Create Account</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
